cddfdaea4ddf1e2f24c51c96a1d3f4c67e4e2301
Joel Lertua Coico <joelcoico17@gmail.com>
2025-10-11 21:26:16 -0500

    Version Update

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -16,6 +16,7 @@ const MusicPlayer = () => {
         album={currentTrack?.album || ""}
         coverUrl={currentTrack?.coverUrl || ""}
       />
+      {console.log(currentTrack)}
       <ProgressBar 
         value={Math.floor(currentTime)}
         maxValue={Math.max(0, Math.floor(duration || 0))}
